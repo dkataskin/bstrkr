8c157b4947f4f0f027e28ae7e2fc1829f887e974
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2014-08-21 23:28:04 +0400

    parse bus13 live data

--- bstrkr.mobile/bstrkr.core/ILiveDataProvider.cs
+++ bstrkr.mobile/bstrkr.core/ILiveDataProvider.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Collections.Immutable;
 
 namespace bstrkr.core
 {
 	public interface ILiveDataProvider
 	{
 		event EventHandler<VehicleLocationsUpdatedEventArgs> VehicleLocationsUpdated;
+
+		IImmutableList<Route> Routes { get; }
+
+		IImmutableList<Vehicle> Vehicles { get; }
 
 		void Start();
 
--- bstrkr.mobile/bstrkr.core/Providers/Bus13LiveDataProvider.cs
+++ bstrkr.mobile/bstrkr.core/Providers/Bus13LiveDataProvider.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
@@ -10,33 +12,78 @@ using Newtonsoft.Json.Linq;
 
 using RestSharp.Portable;
 using RestSharp.Portable.Deserializers;
+
 using Providers;
 
 namespace bstrkr.core.providers
 {
 	public class Bus13LiveDataProvider : ILiveDataProvider
 	{
-		private string _endpoint;
-		private string _location;
+		private readonly string _endpoint;
+		private readonly string _location;
+		private readonly TimeSpan _updateInterval;
+		private readonly IBus13RouteDataService _dataService;
 
-		private IBus13RouteDataService _dataService;
+		private Task _updateTask;
+		private CancellationTokenSource _cancellationTokenSource;
 
-		public Bus13LiveDataProvider(string endpoint, string location)
+		public Bus13LiveDataProvider(string endpoint, string location, TimeSpan updateInterval)
 		{
 			_endpoint = endpoint;
 			_location = location;
+			_updateInterval = updateInterval;
 
 			_dataService = new Bus13RouteDataService(_endpoint, _location);
 		}
 
 		public event EventHandler<VehicleLocationsUpdatedEventArgs> VehicleLocationsUpdated;
 
+		public IImmutableList<Route> Routes 
+		{
+			get 
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public IImmutableList<Vehicle> Vehicles 
+		{
+			get 
+			{
+				throw new NotImplementedException();
+			}
+		}
+
 		public void Start()
 		{
+			//_dataService.GetRoutesAsync();
+
+			_cancellationTokenSource = new CancellationTokenSource();
+			_updateTask = Task.Factory.StartNew(
+										() => this.UpdateInLoop(_dataService, _updateInterval, _cancellationTokenSource.Token), 
+										_cancellationTokenSource.Token);
 		}
 
 		public void Stop()
 		{
+		}
+
+		private void UpdateInLoop(IBus13RouteDataService dataService, TimeSpan sleepInterval, CancellationToken token)
+		{
+			while(!token.IsCancellationRequested)
+			{
+				try
+				{
+					_dataService.GetVehicleLocationsAsync();
+				} 
+				catch (Exception e)
+				{
+				}
+				finally
+				{
+					Task.Delay(sleepInterval).Wait();
+				}
+			}
 		}
 
 		private void RaiseVehicleLocationsUpdatedEvent(IEnumerable<Vehicle> vehicles)
