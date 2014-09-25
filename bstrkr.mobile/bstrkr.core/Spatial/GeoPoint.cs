66f735bb43aa380a4ec1e644d5308755629da1b4
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2014-09-24 20:38:02 -0400

    distance on earth

--- bstrkr.mobile/bstrkr.core/Spatial/GeoPoint.cs
+++ bstrkr.mobile/bstrkr.core/Spatial/GeoPoint.cs
@@ -4,6 +4,8 @@ namespace bstrkr.core.spatial
 {
 	public struct GeoPoint
 	{
+		private const double R = 6371.0d;
+
 		private static GeoPoint EmptyLocation = new GeoPoint(0, 0);
 
 		public GeoPoint(double latitude, double longitude)
@@ -19,6 +21,25 @@ namespace bstrkr.core.spatial
 		public static GeoPoint Empty 
 		{ 
 			get { return EmptyLocation; }
+		}
+
+		public static double ToRadians(double degrees)
+		{
+			return (degrees * Math.PI) / 180;
+		}
+
+		public double Distance(GeoPoint point)
+		{
+			var lat1 = ToRadians(this.Latitude);
+			var lat2 = ToRadians(point.Latitude);
+			var latDelta = ToRadians(point.Latitude - this.Latitude);
+			var lonDelta = ToRadians(point.Longitude - this.Longitude);
+
+			var a = Math.Sin(latDelta / 2.0) * Math.Sin(latDelta / 2.0) +
+			        Math.Cos(lat1) * Math.Cos(lat2) * Math.Sin(lonDelta / 2.0) * Math.Sin(lonDelta / 2.0);
+			var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+			return R * c;
 		}
 
 		public override string ToString()
@@ -42,5 +63,6 @@ namespace bstrkr.core.spatial
 			return this.Latitude.Equals(geoPoint.Latitude) &&
 				   this.Longitude.Equals(geoPoint.Longitude);
 		}
+
 	}
 }
