965ec3f004d9f5ebcea02e848d86ec00d0fc98a4
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2014-09-26 10:14:02 -0400

    sample responses, test provider

---
+++ bstrkr.mobile/bstrkr.tests/Providers/TestProvider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Immutable;
+
+using bstrkr.core;
+using bstrkr.core.interfaces;
+
+namespace bstrkr.tests.providers
+{
+	public class TestProvider : ILiveDataProvider
+	{
+		public TestProvider()
+		{
+		}
+
+		public event EventHandler<VehicleLocationsUpdatedEventArgs> VehicleLocationsUpdated;
+
+		public void Start()
+		{
+		}
+
+		public void Stop()
+		{
+		}
+
+		public IImmutableList<Route> Routes 
+		{
+			get 
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public IImmutableList<Vehicle> Vehicles 
+		{
+			get 
+			{
+				throw new NotImplementedException();
+			}
+		}
+	}
+}
