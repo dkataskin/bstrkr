71eb154fb19384d1360ab1064ce0a2b69db600f5
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2014-10-07 11:17:31 +0400

    display markers on the map

--- bstrkr.mobile/bstrkr.ios/Views/VehicleMarker.cs
+++ bstrkr.mobile/bstrkr.ios/Views/VehicleMarker.cs
@@ -2,12 +2,35 @@
 
 using Google.Maps;
 
+using MonoTouch.CoreLocation;
+using MonoTouch.UIKit;
+
+using bstrkr.mvvm.viewmodels;
+
 namespace bstrkr.ios.views
 {
 	public class VehicleMarker : Marker
 	{
-		public VehicleMarker()
+		private readonly VehicleViewModel _vehicleVM;
+
+		public VehicleMarker(VehicleViewModel vehicleVM)
 		{
+			_vehicleVM = vehicleVM;
+
+			this.Position = new CLLocationCoordinate2D(vehicleVM.Location.Latitude, vehicleVM.Location.Longitude);
+			this.Flat = true;
+			this.Rotation = vehicleVM.VehicleHeading;
+			this.Icon = vehicleVM.Icon as UIImage;
+
+			_vehicleVM.PropertyChanged += this.OnVMPropertyChanged;
+		}
+
+		private void OnVMPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs args)
+		{
+			if (args.PropertyName.Equals("Location"))
+			{
+				this.Position = new CLLocationCoordinate2D(_vehicleVM.Location.Latitude, _vehicleVM.Location.Longitude);
+			}
 		}
 	}
 }
--- bstrkr.mobile/bstrkr.ios/Views/VehicleMarkerManager.cs
+++ bstrkr.mobile/bstrkr.ios/Views/VehicleMarkerManager.cs
@@ -18,15 +18,7 @@ namespace bstrkr.ios.views
 
 		protected override VehicleMarker CreateMarker(object item)
 		{
-			var vehicleVM = item as VehicleViewModel;
-
-			return new VehicleMarker 
-			{
-				Position = new CLLocationCoordinate2D(vehicleVM.Location.Latitude, vehicleVM.Location.Longitude),
-				Flat = true,
-				Rotation = vehicleVM.VehicleHeading,
-				Icon = vehicleVM.Icon as UIImage
-			};
+			return new VehicleMarker(item as VehicleViewModel);
 		}
 	}
 }
