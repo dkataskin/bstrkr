cef0a467c4b2dad9541e98bb540ec6ec457c1e2b
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2014-11-03 19:52:06 +0300

    fragments navigation presenter

---
+++ bstrkr.mobile/bstrkr.android/Helpers/ICustomPresenter.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace bstrkr.android.helpers
+{
+	public interface ICustomPresenter
+	{
+		void Register(Type viewModelType, IFragmentHost host);
+	}
+}
--- bstrkr.mobile/bstrkr.android/Helpers/IFragmentHost.cs
+++ bstrkr.mobile/bstrkr.android/Helpers/IFragmentHost.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 using Cirrious.MvvmCross.ViewModels;
 
 namespace bstrkr.android.helpers
