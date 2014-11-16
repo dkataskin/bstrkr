d8493f01325b78f16ab6ec4ae5f590086efad0e0
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2014-11-16 20:49:35 +0300

    settings vm

--- bstrkr.mobile/bstrkr.mvvm/ViewModels/SettingsViewModel.cs
+++ bstrkr.mobile/bstrkr.mvvm/ViewModels/SettingsViewModel.cs
@@ -2,9 +2,12 @@
 
 using Cirrious.MvvmCross.ViewModels;
 
+using bstrkr.core.config;
+
 namespace bstrkr.mvvm.viewmodels
 {
 	public class SettingsViewModel : BusTrackerViewModelBase
 	{
+		public BusTrackerLocation Location { get; set; }
 	}
 }
