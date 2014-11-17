e7a063ae33834e1e8ab07252167346e5453ecfa9
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2014-11-17 19:02:21 +0300

    routes

---
+++ bstrkr.mobile/bstrkr.mvvm/ViewModels/RouteViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Cirrious.MvvmCross.ViewModels;
+
+namespace bstrkr.mvvm.viewmodels
+{
+	public class RouteViewModel : MvxViewModel
+	{
+		public RouteViewModel()
+		{
+		}
+	}
+}
--- bstrkr.mobile/bstrkr.mvvm/ViewModels/RoutesViewModel.cs
+++ bstrkr.mobile/bstrkr.mvvm/ViewModels/RoutesViewModel.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 
 namespace bstrkr.mvvm.viewmodels
 {
 	public class RoutesViewModel : BusTrackerViewModelBase
 	{
+		private readonly ObservableCollection<RouteViewModel> _routes = new ObservableCollection<RouteViewModel>();
+
+		public RoutesViewModel()
+		{
+			this.Routes = new ReadOnlyObservableCollection<RouteViewModel>(_routes);
+		}
+
+		public ReadOnlyObservableCollection<RouteViewModel> Routes { get; private set; }
 	}
 }
