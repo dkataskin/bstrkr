53c48c2f3766b9593d88165047d87f50f9c7c6a8
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2014-11-18 17:46:58 +0300

    route viewmodel

--- bstrkr.mobile/bstrkr.mvvm/ViewModels/RouteViewModel.cs
+++ bstrkr.mobile/bstrkr.mvvm/ViewModels/RouteViewModel.cs
@@ -6,8 +6,27 @@ namespace bstrkr.mvvm.viewmodels
 {
 	public class RouteViewModel : MvxViewModel
 	{
+		private string _name;
+
 		public RouteViewModel()
 		{
+		}
+
+		public string Name 
+		{ 
+			get
+			{
+				return _name;
+			} 
+
+			set
+			{
+				if (!string.Equals(_name, value))
+				{
+					_name = value;
+					this.RaisePropertyChanged(() => this.Name)
+				};
+			} 
 		}
 	}
 }
