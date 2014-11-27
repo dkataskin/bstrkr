4467017989d7b16d906dc7e160507c799fa36903
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2014-11-27 20:30:33 +0300

    about view

--- bstrkr.mobile/bstrkr.android/Views/AboutView.cs
+++ bstrkr.mobile/bstrkr.android/Views/AboutView.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 
+using Android.App;
+using Android.OS;
+
+using Cirrious.MvvmCross.Binding.Droid.BindingContext;
 using Cirrious.MvvmCross.Droid.Fragging.Fragments;
 
 namespace bstrkr.android.views
 {
-	public class AboutView : MvxFragment
+	public class AboutView : MvxDialogFragment
 	{
-		public AboutView()
+		public override Dialog OnCreateDialog(Bundle savedState)
 		{
+			base.EnsureBindingContextSet(savedState);
+
+			var view = this.BindingInflate(Resource.Layout.fragment_about_view, null);
+
+			var dialog = new AlertDialog.Builder(Activity);
+			dialog.SetTitle(Resources.GetString(Resource.String.about_view_title));
+			dialog.SetView(view);
+			dialog.SetNegativeButton("Cancel", (s, a) => { });
+			dialog.SetPositiveButton("OK", (s, a) => { });
+
+			return dialog.Create();
 		}
 	}
 }
