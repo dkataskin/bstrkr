c68847c1e8a3539e9bf6b83d2bb2e51e3e8235a5
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2014-11-27 21:45:20 +0300

    visibility plugin

---
+++ bstrkr.mobile/bstrkr.android/Bootstrap/VisibilityPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace bstrkr.android.bootstrap
+{
+    public class VisibilityPluginBootstrap
+        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.Visibility.PluginLoader>
+    {
+    }
+}
