472f655bf29d9a68dd8309ef6f7fb5a40ffbf48a
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2014-11-29 18:46:09 +0300

    unknown location dialog

---
+++ bstrkr.mobile/bstrkr.android/Views/UnknownLocationView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Cirrious.MvvmCross.Droid.Fragging.Fragments;
+
+namespace bstrkr.android.views
+{
+	public class UnknownLocationView : MvxDialogFragment
+	{
+		public UnknownLocationView()
+		{
+		}
+	}
+}
