e26f0b797063eff8887822a7e978b9179df85e1e
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2014-12-15 22:12:08 +0300

    missing files

--- bstrkr.mobile/bstrkr.android/Setup.cs
+++ bstrkr.mobile/bstrkr.android/Setup.cs
@@ -1,14 +1,20 @@
+using System.Reflection;
+using System.Resources;
+
 using Android.Content;
 
 using Cirrious.CrossCore;
+using Cirrious.CrossCore.Converters;
 using Cirrious.CrossCore.Platform;
 using Cirrious.MvvmCross.Binding.Bindings.Target.Construction;
 using Cirrious.MvvmCross.Droid.Platform;
 using Cirrious.MvvmCross.Droid.Views;
+using Cirrious.MvvmCross.Localization;
 using Cirrious.MvvmCross.ViewModels;
 
 using Xamarin;
 
+using bstrkr.core;
 using bstrkr.core.android.config;
 using bstrkr.core.android.presenters;
 using bstrkr.core.android.services;
@@ -18,7 +24,9 @@ using bstrkr.core.config;
 using bstrkr.core.services.location;
 using bstrkr.core.services.resources;
 using bstrkr.mvvm;
+using bstrkr.mvvm.localization;
 using bstrkr.mvvm.views;
+using bstrkr.providers;
 using bstrkr.android.views;
 
 namespace bstrkr.android
@@ -27,14 +35,18 @@ namespace bstrkr.android
     {
         public Setup(Context applicationContext) : base(applicationContext)
         {
-			Insights.Initialize("<your_key_here>", applicationContext);
+			Insights.Initialize("<xamarin_insights_key>", applicationContext);
         }
 
 		protected override void InitializeFirstChance()
 		{
+			Mvx.RegisterSingleton(new ResxTextProvider(AppResources.ResourceManager));
+
 			Mvx.LazyConstructAndRegisterSingleton<IConfigManager, ConfigManager>();
 			Mvx.LazyConstructAndRegisterSingleton<ILocationService, SuperLocationService>();
-			Mvx.LazyConstructAndRegisterSingleton<IResourceManager, ResourceManager>();
+			Mvx.LazyConstructAndRegisterSingleton<IAppResourceManager, AndroidAppResourceManager>();
+			Mvx.LazyConstructAndRegisterSingleton<IBusTrackerLocationService, BusTrackerLocationService>();
+			Mvx.LazyConstructAndRegisterSingleton<ILiveDataProviderFactory, DefaultLiveDataProviderFactory>();
 			Mvx.RegisterSingleton<ICustomPresenter>(new CustomPresenter());
 
 			base.InitializeFirstChance();
@@ -48,6 +60,10 @@ namespace bstrkr.android
         protected override IMvxTrace CreateDebugTrace()
         {
             return new DebugTrace();
+
+//			var assembly = typeof(Resources.Strings.Strings).Assembly;
+//			foreach (var res in assembly.GetManifestResourceNames()) 
+//				System.Diagnostics.Debug.WriteLine("found resource: " + res);
         }
 
 		protected override void FillTargetFactories(IMvxTargetBindingFactoryRegistry registry)
@@ -62,6 +78,12 @@ namespace bstrkr.android
 		protected override IMvxAndroidViewPresenter CreateViewPresenter()
 		{
 			return Mvx.Resolve<ICustomPresenter>();
+		}
+
+		protected override void FillValueConverters(IMvxValueConverterRegistry registry)
+		{
+			base.FillValueConverters(registry);
+			registry.AddOrOverwrite("Language", new MvxLanguageConverter());
 		}
     }
 }
