022952ec067ca7dab7ceca18b16b8b3b50315e57
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2015-01-25 11:57:58 +0300

    umbrella route view

--- bstrkr.mobile/bstrkr.mvvm/Converters/VehicleTypeToImageConverter.cs
+++ bstrkr.mobile/bstrkr.mvvm/Converters/VehicleTypeToImageConverter.cs
@@ -1,20 +1,31 @@
 ﻿using System;
 using System.Globalization;
 
+using bstrkr.core;
+using bstrkr.core.services.resources;
+
+using Cirrious.CrossCore;
 using Cirrious.CrossCore.Converters;
 
 namespace bstrkr.mvvm.converters
 {
 	public class VehicleTypeToImageConverter : IMvxValueConverter
 	{
+		private IAppResourceManager _resourceManager;
+
+		public VehicleTypeToImageConverter()
+		{
+			_resourceManager = Mvx.Resolve<IAppResourceManager>();
+		}
+
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			throw new NotImplementedException();
+			return _resourceManager.GetVehicleTypeIcon((VehicleTypes)value);
 		}
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			throw new NotImplementedException();
+			return _resourceManager.GetVehicleTypeIcon((VehicleTypes)value);
 		}
 	}
 }
