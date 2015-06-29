6709585ad7d5634a672ac6d2bbe7b8d009d93d45
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2015-06-29 21:35:41 +0300

    styling

---
+++ bstrkr.mobile/bstrkr.android/Views/CustomImageView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+using Android.Util;
+using Android.Graphics;
+
+namespace bstrkr.android.views
+{
+	[Register("bstrkr.android.views.CustomImageView")]
+	public class CustomImageView : ImageView
+	{
+		public CustomImageView(Context context, IAttributeSet attrs)
+			: base(context, attrs)
+		{
+		}
+
+		public CustomImageView(Context context)
+			: base(context)
+		{
+		}
+
+		protected CustomImageView(IntPtr javaReference, JniHandleOwnership transfer)
+			: base(javaReference, transfer)
+		{
+		}
+
+		public Color TintColor 
+		{
+			get { return new Color(); }
+			set 
+			{
+				this.SetColorFilter(value);
+			}
+		}
+	}
+}
