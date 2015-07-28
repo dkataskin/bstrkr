ccc290b39b6529742e54057f4d60c7b5d1965597
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2015-07-28 13:49:55 +0300

    clean up iOs project

--- bstrkr.mobile/bstrkr.ios/Bootstrap/VisibilityPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using Cirrious.CrossCore.Plugins;
-
-namespace bstrkr.ios.Bootstrap
-{
-    public class VisibilityPluginBootstrap
-        : MvxLoaderPluginBootstrapAction<Cirrious.MvvmCross.Plugins.Visibility.PluginLoader, Cirrious.MvvmCross.Plugins.Visibility.Touch.Plugin>
-    {
-    }
-}
