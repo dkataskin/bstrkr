0ddfb4f97f606836c7076e9bf0e7e5bb749733ca
Dmitriy Kataskin <dmitriy.kataskin@gmail.com>
2015-08-10 18:34:59 +0300

    viewport changed message

---
+++ bstrkr.mobile/bstrkr.mvvm/Messages/MainViewportChangedMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Cirrious.MvvmCross.Plugins.Messenger;
+
+namespace bstrkr.mvvm.messages
+{
+	public class MainViewportChangedMessage : MvxMessage
+	{
+		public MainViewportChangedMessage(float visibleAreaOffset)
+		{
+			this.VisibleAreaOffset = visibleAreaOffset;
+		}
+
+		public float VisibleAreaOffset { get; set; }
+	}
+}
